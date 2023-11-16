ff6b334f828eeced90a5b2dcb0e2879300539064
testing <snvatesting@gmail.com>
2023-11-16 11:00:02 +0530

    hi

--- src/StockMarket.java
+++ src/StockMarket.java
@@ -5,7 +5,7 @@ public class StockMarket {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of an array: ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int [] arr = new int[n];
         System.out.println("Enter the array element: ");
 
         for(int i = 0; i < n; i++){
